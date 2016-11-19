637973d96d7c60c8906a55cabb84a4603fda6c17
fhh <281535508@qq.com>
2016-11-19 15:23:02 +0800

    delete

--- xunzhen -修改/js/filing.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	$('input').attr("disabled",true);
-	$('.personInfo_righttop .save').click(function(){
-		$('input').attr("disabled",false);
-		$(this).css('background-image','url(./images/save.png)')
-	})
-})
